a950726ae482667db40b22956f3148415f0ba430
2022212045 <117084491+2022212045@users.noreply.github.com>
2023-01-26 14:17:53 +0800

    6
    Signed-off-by: 2022212045 <117084491+2022212045@users.noreply.github.com>

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.weather;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
@@ -18,12 +21,14 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     private Button mbutton;
     private TextView mtv;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        mHandler = new MyHandler();//准备接线员
         mbutton.setOnClickListener(view -> sendGetNetRequest());
 
     }
@@ -44,13 +49,13 @@ public class MainActivity extends AppCompatActivity {
                         connection.connect();//正式连接
                         InputStream in = connection.getInputStream();//从接口处获取
                         String responseData = StreamToString(in);//这里就是服务器返回的数据
-                        MyHandler mHandler = new MyHandler();
                         //下面三行代码的意思就是将数据保存在message里也就是消息里，然后交给接线员
                         Message message = new Message();//新建一个Message
                         message.obj = responseData;//将数据赋值给message的obj属性
                         mHandler.sendMessage(message);
-                        mtv.setText("responseData");
-//                        Log.d("RQ", "sendGetNetRequest: "+responseData);
+//                        a.setText("String"+responseData+"String");
+//                        mtv.setText("String"+responseData+"String");
+                        Log.d("RQ", "sendGetNetRequest: " + responseData);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
@@ -61,24 +66,9 @@ public class MainActivity extends AppCompatActivity {
         StringBuilder sb = new StringBuilder();//新建一个StringBuilder，用于一点一点
         String oneLine;//流转换为字符串的一行
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));//
-        int time=0;
         try {
-//            (oneLine = reader.readLine()).indexOf("℃")==-1
-            while (reader.readLine() != null) {
-                //readLine方法将读取一行
-                if ((oneLine = reader.readLine()).contains("℃")) {
-                } else {
-                    if (time == 0) {
-                        sb.append('最').append('高').append(oneLine).append('\n');
-                    } else {
-                        sb.append('最').append('低').append(oneLine).append('\n');
-                    }
-                    time++;
-                    if (time > 1) {
-                        break;
-                    }
-                }
-                //拼接字符串并且增加换行，提高可读性
+            while ((oneLine = reader.readLine()) != null) {//readLine方法将读取一行
+                sb.append(oneLine).append('\n');//拼接字符串并且增加换行，提高可读性
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -92,11 +82,80 @@ public class MainActivity extends AppCompatActivity {
         return sb.toString();//将拼接好的字符串返回出去
     }
 
+//    private String StreamToString(InputStream in) {
+//        StringBuilder sb = new StringBuilder();//新建一个StringBuilder，用于一点一点
+//        String oneLine;//流转换为字符串的一行
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(in));//
+//        int time=0;
+//        try {
+////            (oneLine = reader.readLine()).indexOf("℃")==-1
+//            while (reader.readLine() != null) {
+//                //readLine方法将读取一行
+//                if ((oneLine = reader.readLine()).contains("℃")) {
+//                } else {
+//                    if (time == 0) {
+//                        sb.append('最').append('高').append(oneLine).append('\n');
+//                    } else {
+//                        sb.append('最').append('低').append(oneLine).append('\n');
+//                    }
+//                    time++;
+//                    if (time > 1) {
+//                        break;
+//                    }
+//                }
+//                //拼接字符串并且增加换行，提高可读性
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                in.close();//关闭InputStream
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        return sb.toString();//将拼接好的字符串返回出去
+//    }
+
     private void initView() {
         mtv = findViewById(R.id.textView);
         mbutton = findViewById(R.id.button);
     }
 
+    @SuppressLint("SetTextI18n")
+    private void jsonDecodeTest(String jsonData) {
+        //原始json字段
+//        String jsonData0 = "{\"redrock\":{\"woman_num\": 5,\"man_num\": 0},\"man\":[{\"name\":\"王鸿杨\",\"status\":\"摸鱼\"},{\"name\":\"艾卫熹\",\"status\":\"刷leetcode\"},{\"name\":\"郭晓强\",\"status\":\"手撸compose\"},{\"name\":\"李戬\",\"status\":\"写自定义view\"},{\"name\":\"郭涵宇\",\"status\":\"写博客\"},]}";
+        String str = jsonData.replaceAll("\\D+", "");//提取数字
+        mtv.setText("String" + str + "String");
+//            int time=0;
+//            StringBuilder sb = new StringBuilder();
+//            while (jsonData.readLine() != null) {
+//                //readLine方法将读取一行
+//                if ((oneLine = jsonData.readLine()).contains("℃")) {
+//                } else {
+//                    if (time == 0) {
+//                        sb.append('最').append('高').append(oneLine).append('\n');
+//                    } else {
+//                        sb.append('最').append('低').append(oneLine).append('\n');
+//                    }
+//                    time++;
+//                    if (time > 1) {
+//                        break;
+//                    }
+//                }
+//            }
+    }
 
-
+    @SuppressLint("HandlerLeak")
+    private class MyHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            //这个Message msg 就是从另一个线程传递过来的数据
+            //在这里进行你要对msg的处理
+            String responseData = msg.obj.toString();
+            jsonDecodeTest(responseData);
+        }
+    }
 }
